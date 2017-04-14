44dc8d9455987f1d8f77bd01088db3381a4c0a2e
motih <Motih.Tutorial@gmail.com>
2017-04-14 15:31:23 +0300

    reqirejs

---
+++ Modules/Modules/CommonJS/JavaScript.js
@@ -0,0 +1,6 @@
+﻿var example = require(['./lib.js'], function (lib) {
+    var h = lib.hello();
+    document.getElementById("h").innerHTML = h;
+});
+
+//return;
---
+++ Modules/Modules/CommonJS/lib.js
@@ -0,0 +1,11 @@
+﻿define(function () {
+    return {
+        hello: function () {
+            return 'Hello World'
+        }
+    };
+});
+
+//exports.hello = function () {
+//  return 'Hello World'
+//};
